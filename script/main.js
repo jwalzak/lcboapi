5b3d6fc9a4f63d91489bb2fe74ae30a3fe6b149d
Jason Walzak <walzak@gmail.com>
2017-07-06 01:18:59 -0400

    The application now creates a dropdown menu when loading the site. The menu is created dynamically with the results of the lcboapi query. Currentyly showing Beer, Ciders, Spirits.

--- script/main.js
+++ script/main.js
@@ -18,14 +18,28 @@ function getJSON(url) {
 
 let data = getJSON(`https://lcboapi.com/products?access_key=${ ACCESS_KEY }`);
 let parsedJSON = JSON.parse(data);
+const types = parsedJSON.result;
+const liquorType = [...new Set(types.map(item => item.primary_category))];
+let select = document.createElement('select'),
+    option,
+    i = 0;
 
-for(let i = 0; i<parsedJSON.result.length; i++){
-  if(parsedJSON.result[i].primary_category == "Beer"){
-    let value = parsedJSON.result[i];
-    let price = (value.price_in_cents/100).toFixed(2);
-    let name = value.name;
-    let container = value.package_unit_type;
-    let amount = value.total_package_units;
-    document.writeln(price + " " + name + " " + container + " " + amount);
-  }
+for(; i < liquorType.length; i++){
+  option = document.createElement('option');
+  option.setAttribute('value', liquorType[i]);
+  option.appendChild(document.createTextNode(liquorType[i]));
+  select.appendChild(option);
 }
+
+document.body.appendChild(select);
+
+// for(let i = 0; i<parsedJSON.result.length; i++){
+//   if(parsedJSON.result[i].primary_category == "Beer"){
+//     let value = parsedJSON.result[i];
+//     let price = (value.price_in_cents/100).toFixed(2);
+//     let name = value.name;
+//     let container = value.package_unit_type;
+//     let amount = value.total_package_units;
+//     document.writeln(price + " " + name + " " + container + " " + amount + "<br/>");
+//   }
+// }
